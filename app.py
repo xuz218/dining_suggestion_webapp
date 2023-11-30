666c9237d46d581c9c3f5c14e8e596dd35e532ee
Ziang Xu <xuz218@wfu.edu>
2023-11-30 14:07:27 -0500

    update

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request, render_template, g, redirect, Response, abort, url_for, session, app
+from flask_login import logout_user
+import os
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
-app = Flask(__name__, static_folder='front-end')
+# app = Flask(__name__, static_folder='front-end')
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__)
+app.secret_key = "secretkey"
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+
+def save_user_to_dynamodb(user_data):
+    table = dynamodb.Table('User')
+    try:
+        response = table.put_item(Item=user_data)
+        return response
+    except Exception as e:
+        print(f"Error saving user: {str(e)}")
+        return None
 
 @app.route('/')
 def index():
