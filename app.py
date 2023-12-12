eab0747d56121b550d0c675f4a899aa1fe2d8951
Ziang Xu <xuz218@wfu.edu>
2023-12-12 15:12:46 -0500

    update

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, send_from_directory, request, render_template, g, redirect, Response, abort, url_for, session, app
-from flask_login import logout_user
+# from flask_login import logout_user
 import os
 import boto3
-from boto3.dynamodb.conditions import Key, Attr
+# from boto3.dynamodb.conditions import Key, Attr
 
 # app = Flask(__name__, static_folder='front-end')
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
@@ -22,27 +22,27 @@ def save_user_to_dynamodb(user_data):
 
 @app.route('/')
 def index():
-    return send_from_directory(app.static_folder, 'index.html')
+    return render_template('index.html')
 
 @app.route('/login')
 def login():
-    return send_from_directory(app.static_folder, 'login.html')
+    return render_template('login.html')
 
 @app.route('/profile')
 def profile():
-    return send_from_directory(app.static_folder, 'profile.html')
+    return render_template('profile.html')
 
 @app.route('/rating')
 def rating():
-    return send_from_directory(app.static_folder, 'rating.html')
+    return render_template('rating.html')
 
 @app.route('/search')
 def search():
-    return send_from_directory(app.static_folder, 'search.html')
+    return render_template('search.html')
 
 @app.route('/signup')
 def signup():
-    return send_from_directory(app.static_folder, 'signup.html')
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
